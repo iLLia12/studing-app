82c2dec8a40b99333c6d36ce478ad130f5d4d617
illia goncharov <goncharovphp@gmail.com>
2024-07-22 23:02:57 -0700

    working on styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,12 @@ import Xterm from "../components/xterm";
 import MonacoEditor from "../components/monaco-editor";
 import { ProgrammingLanguageOptions } from "../components/monaco-editor/types";
 import FooterComponent from "../components/footer";
+import GolangIcon from "../components/svg/golang/icon-2.svg";
+import PythonIcon from "../components/svg/python/icon-3.svg";
+import JavaScriptIcon from "../components/svg/js/icon-1.svg";
+import PhpIcon from "../components/svg/php/icon-1.svg";
 import { useState } from "react";
+import Image from "next/image";
 
 //const heigth = "50px";
 
@@ -22,24 +27,41 @@ export default function Home() {
     console.log("The code is being executed: ", code);
   }
 
-  const handleKeyDown = (e: any) => {
-    if ((e.metaKey || e.ctrlKey) && e.key === "s") {
-      e.preventDefault();
-      //@ts-ignore
-      onExecute(editorRef.current?.getValue());
-    }
-  };
-
   return (
     <div className="min-h-screen vs-dark-bg">
       <div className={`w-full flex h-[calc(100vh-50px)]`}>
-        <div className="bg-zinc-900 min-w-20 dark-bg-sec">
-          <div className="w-full grid grid-rows-12 h-full vs-dark-bg">
-            <button className="bg-slate-400 text-center m-1">Add lang</button>
-            <button className="bg-slate-400 text-center m-1">Drawing</button>
-            <button className="bg-slate-400 text-center m-1">Drawing</button>
-            <button className="bg-slate-400 text-center m-1">Drawing</button>
-            <button className="bg-slate-400 text-center m-1">Drawing</button>
+        <div className="min-w-20 dark-bg-sec">
+          <div className="w-full grid grid-rows-12 h-full vs-dark-bg pt-1">
+            <button className="dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 hover:shadow-stone-600 shadow-md">
+              <Image
+                priority
+                src={GolangIcon}
+                height={33}
+                width={33}
+                alt="Golang"
+              />
+            </button>
+            <button className="dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 hover:shadow-stone-600 shadow-md">
+              <Image
+                priority
+                src={PythonIcon}
+                height={30}
+                width={30}
+                alt="Python"
+              />
+            </button>
+            <button className="dark-bg-normal text-center m-1 rounded flex justify-center items-center transition ease-in-out duration-200 hover:shadow-stone-600 shadow-md">
+              <Image
+                priority
+                src={JavaScriptIcon}
+                height={27}
+                width={27}
+                alt="Javascript"
+              />
+            </button>
+            <button className="dark-bg-normal text-center m-1 rounded flex justify-center items-center transition ease-in-out duration-200 hover:shadow-stone-600 shadow-md">
+              <Image priority src={PhpIcon} height={33} width={33} alt="PHP" />
+            </button>
           </div>
         </div>
         <div className="w-full">
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,12 +1,28 @@
+import Image from "next/image";
+import ProgrammingLangIcon from "../../components/svg/programming-lang/icon-1.svg";
+import WhiteboardIcon from "../../components/svg/whiteboard/icon-1.svg";
+
 export default function Footer() {
   return (
     <div className={`fixed w-full bg-zinc-900 h-[50px] dark-bg-sec`}>
       <div className="w-full grid grid-cols-12 gap-2 h-full">
-        <button className="bg-slate-400 text-center m-1 col-start-6 col-end-7">
-          Add lang
+        <button className="dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 hover:shadow-stone-600 shadow-md">
+          <Image
+            priority
+            src={ProgrammingLangIcon}
+            height={30}
+            width={30}
+            alt="Programming lang"
+          />
         </button>
-        <button className="bg-slate-400 text-center m-1 col-start-7 col-end-8">
-          Drawing
+        <button className="dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 hover:shadow-stone-600 shadow-md">
+          <Image
+            priority
+            src={WhiteboardIcon}
+            height={30}
+            width={30}
+            alt="Whiteboard"
+          />
         </button>
       </div>
     </div>
