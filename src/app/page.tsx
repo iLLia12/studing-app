ece76dfe035f862addb43c2d3c6c5708ab2a5031
illia goncharov <goncharovphp@gmail.com>
2024-08-29 20:41:54 -0700

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,10 +13,34 @@ import Image from "next/image";
 import { WriteToTerminalHandler } from "../components/xterm/";
 
 const langToIconMap = {
-  [ProgrammingLanguageOptions.GO]: GolangIcon,
-  [ProgrammingLanguageOptions.PYTHON]: PythonIcon,
-  [ProgrammingLanguageOptions.JAVASCRIPT]: JavaScriptIcon,
-  [ProgrammingLanguageOptions.JAVA]: JavaIcon,
+  [ProgrammingLanguageOptions.GO]: {
+    icon: GolangIcon,
+    size: {
+      height: 35,
+      width: 35,
+    },
+  },
+  [ProgrammingLanguageOptions.PYTHON]: {
+    icon: PythonIcon,
+    size: {
+      height: 30,
+      width: 30,
+    },
+  },
+  [ProgrammingLanguageOptions.JAVASCRIPT]: {
+    icon: JavaScriptIcon,
+    size: {
+      height: 23,
+      width: 23,
+    },
+  },
+  [ProgrammingLanguageOptions.JAVA]: {
+    icon: JavaIcon,
+    size: {
+      height: 30,
+      width: 30,
+    },
+  },
 };
 
 export default function Home() {
@@ -47,71 +71,35 @@ export default function Home() {
 
   function activeBtn(l: ProgrammingLanguageOptions) {
     if (l == lang) {
-      return "shadow-stone-600 shadow-inner";
+      return "dark-bg-normal";
     }
     return "";
   }
 
   return (
-    <div className="min-h-screen vs-dark-bg">
+    <div className="min-h-screen">
       <div className={`w-full flex h-[calc(100vh-50px)]`}>
-        <div className="min-w-20 dark-bg-sec">
-          <div className="w-full grid grid-rows-12 h-full vs-dark-bg pt-1">
+        <div className="min-w-20">
+          <div className="w-full h-full dark-bg-sec">
             {Object.values(ProgrammingLanguageOptions).map(
               (lang: ProgrammingLanguageOptions) => (
                 <button
                   key={lang}
                   className={
-                    `dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 ` +
+                    `dark-bg-sec border-b border-neutral-700 w-full min-h-16 text-center flex justify-center items-center transition ease-in-out duration-200 ` +
                     activeBtn(lang)
                   }
                 >
                   <Image
                     priority
-                    src={langToIconMap[lang]}
-                    height={23}
-                    width={23}
+                    src={langToIconMap[lang].icon}
+                    height={langToIconMap[lang].size.height}
+                    width={langToIconMap[lang].size.width}
                     alt={lang}
                   />
                 </button>
               )
             )}
-            {/* <button className="dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 shadow-stone-700 shadow-inner">
-              <Image
-                priority
-                src={GolangIcon}
-                height={33}
-                width={33}
-                alt="Golang"
-              />
-            </button>
-            <button className="dark-bg-normal text-center rounded m-1 flex justify-center items-center transition ease-in-out duration-200 shadow-stone-700 shadow-inner">
-              <Image
-                priority
-                src={PythonIcon}
-                height={30}
-                width={30}
-                alt="Python"
-              />
-            </button>
-            <button className="dark-bg-normal text-center m-1 rounded flex justify-center items-center transition ease-in-out duration-200 shadow-stone-700 shadow-inner">
-              <Image
-                priority
-                src={JavaScriptIcon}
-                height={27}
-                width={27}
-                alt="Javascript"
-              />
-            </button>
-            <button className="dark-bg-normal text-center m-1 rounded flex justify-center items-center transition ease-in-out duration-200 shadow-stone-700 shadow-inner">
-              <Image
-                priority
-                src={JavaIcon}
-                height={30}
-                width={30}
-                alt="Java"
-              />
-            </button> */}
           </div>
         </div>
         <div className="w-full">
